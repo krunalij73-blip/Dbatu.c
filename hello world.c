290cf4d1a58dbbf436e282e6f24e188cb481ce39
Krunali Joshi <krunalij73@gmail.com>
2026-05-05 21:24:22 +0530

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,12 @@
+/*PRN-2503033111378UD005
+ bhavesh Vijay.chitriv
+  VLSI design */
+ 
+#include(stdio.h)
+#include(conio.h)
+int main()
+{
+	printf("hello world");
+	printf("hello me");
+	return 0;
+}
