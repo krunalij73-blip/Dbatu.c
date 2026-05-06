17bb1d630b6801e3fa1acbf3b1e232652859bf2e
Krunali Joshi <krunalij73@gmail.com>
2026-05-07 01:11:57 +0530

    Add files via upload

---
+++ substraction.c
@@ -0,0 +1,25 @@
+
+/*Name:- Krunali Devanand Joshi
+PRN:-2503033111378UD009*/
+
+
+
+#include <stdio.h>
+
+int main()
+
+{
+	int num1,num2,sub1;
+	
+	printf("Enter first number :\t");
+	scanf("%d",&num1);
+	
+	printf("Enter second number :\t");
+	scanf("%d",&num2);
+	
+	sub1=num1-num2;
+	printf("sub is %d",sub1);
+	
+	
+	return 0;
+}
