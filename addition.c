092aa12a974afdd8984d55a2782b163bdaddbba0
Krunali Joshi <krunalij73@gmail.com>
2026-05-07 01:11:08 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,27 @@
+
+/* Name:- Krunali Devanand Joshi 
+PRN:-2503033111378UD009*/
+
+
+
+
+
+#include <stdio.h>
+
+int main()
+
+{
+	int num1,num2,sum1;
+	
+	printf("Enter the first number :\t");
+	scanf("%d",&num1);
+	
+	printf("Enter the second number :\t");
+	scanf("%d",&num2);
+	
+	sum1=num1+num2;
+	printf("sum is %d\n",sum1);
+	
+	
+	return 0;
+}
