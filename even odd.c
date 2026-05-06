df3d31953d6b03b2ba149280d7f425de075a7c9a
Krunali Joshi <krunalij73@gmail.com>
2026-05-07 01:12:33 +0530

    Add files via upload

---
+++ even odd.c
@@ -0,0 +1,30 @@
+/*Name:-Krunali Devanand Joshi
+PRN:-2503033111378UD009*/
+
+
+
+#include <stdio.h>
+
+int main()
+
+{
+	int num,rem;
+	
+	printf("Enter number :\t");
+	scanf("%d",&num);
+	
+	rem=num%2;
+	
+	if(rem==0)
+	
+	{
+		printf("Given number is EVEN");
+		
+	 } 
+	 else
+	 { printf("Given number is ODD");
+	 	
+	 }
+	 
+	 return 0;
+}
