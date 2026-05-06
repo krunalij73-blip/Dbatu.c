b964b5b5cda24404835cc444dae85bb81fe812df
Krunali Joshi <krunalij73@gmail.com>
2026-05-07 01:13:05 +0530

    Add files via upload

---
+++ pin.c
@@ -0,0 +1,28 @@
+
+/*Name:-Krunali Devanand Joshi
+PRN:-2503033111378UD009*/
+
+
+
+#include <stdio.h>
+
+int main()
+
+{
+	int S_pin,U_pin;
+	
+	S_pin=1135;
+	
+	printf("Eter the PIN : \t");
+	scanf("%d",&U_pin);
+	
+	if(S_pin==U_pin){
+		
+		printf("CORRECT PIN");
+	}
+	else
+	{ printf("INCORRECT PIN");
+		
+	}
+	
+}
